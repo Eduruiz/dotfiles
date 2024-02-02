a07d8f9d507b489e120ef5688464b980f8a2eb75
Edu Ruiz <eduardo.ruiz@vuxx.com.br>
2024-02-02 12:44:03 -0300

    Add my fork of vim-blade for blade files syntax
    This needs to get better with time, add some LSP configurations os
    something like that

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -44,6 +44,9 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  -- vim blade syntax highlight
+  'Eduruiz/vim-blade',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
