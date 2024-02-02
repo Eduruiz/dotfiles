f8bec6bfefee31c48a39e0ffcc14ef03eab7500d
Edu Ruiz <eduardo.ruiz@vuxx.com.br>
2024-02-02 18:20:44 -0300

    Add devicons to lualine and config it more the way I like it

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -185,12 +185,32 @@ require('lazy').setup({
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    },
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'auto',
         component_separators = '|',
         section_separators = '',
+      },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1, -- relative path
+          }
+        }
+      },
+      tabline = {
+        lualine_a = {'buffers'},
+        lualine_c = {
+          {
+            'filename',
+            path = 0, -- just file name
+          }
+        },
       },
     },
   },
