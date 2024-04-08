738614e99181d1cf7ab47ede0a91ce90fa39a10c
Edu Ruiz <eduardo.ruiz@vuxx.com.br>
2024-04-08 18:08:02 -0300

    Add tags search to telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -699,6 +699,7 @@ vim.keymap.set('n', '<leader>sg', telescope_live_grep, { desc = '[S]earch by [G]
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags, { desc = '[S]earch [T]Tags' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
